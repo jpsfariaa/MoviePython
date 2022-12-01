49052366fb1251fa4c80a0310f61212bcaa7c409
jpsfariaa <jpsanfaria@gmail.com>
2022-12-01 02:17:43 -0300

    Create iniciar_app.py
    Configurações do Aplicativo para o Flask

---
+++ Code/iniciar_app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# Configurações do Aplicativo para o Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'moviepython'
+app.config['SQLALCHEMY_DATABASE_URI'] = '{SGBD}://{user}:{password}@{server}/{database}'.format(
+    SGBD = 'mysql+mysqlconnector',
+    user = 'root',
+    password = '12345',
+    server = 'localhost',
+    database = 'bancofilmes'
+)
+db = SQLAlchemy(app)
+
+# Importando as Views
+from views import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
